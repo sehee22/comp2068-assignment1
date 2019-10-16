cd818816862b58bbddfb10f9bef5c595ccde2d36
Sehee Park <200389459@student.georgianc.on.ca>
2019-10-16 11:13:00 -0400

    first commit

---
+++ Assignment1/routes/index.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Home' });
+});
+
+router.get('/:page', function (req, res) {
+    if (req.params.page == 'index') {
+        res.render('index', { title: 'Home' });
+    }  
+    else if (req.params.page == 'about') {
+        res.render('about', { title: 'About Me' });
+    }      
+    else if (req.params.page == 'projects') {
+        res.render('projects', { title: 'Projects' });
+    }    
+    else if (req.params.page == 'services') {
+        res.render('services', { title: 'Services' });
+    }  
+    else if (req.params.page == 'contact') {
+        res.render('contact', { title: 'Contact Me' });
+    }
+});
+
+module.exports = router;
